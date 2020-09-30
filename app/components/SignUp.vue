88f882145933876ac84d90324328b81164885ad1
Triaro <abhimehra2762@gmail.com>
2020-09-30 14:59:18 +0530

    final

--- app/components/SignUp.vue
+++ app/components/SignUp.vue
@@ -66,55 +66,6 @@ export default {
 </script>
 
 <style lang="scss">
-  .border-bottom {
-    border-bottom-width: 1;
-    border-bottom-color: white;
-    margin-bottom: 40;
-    padding-bottom: 8;
-  }
-
-  .form-input {
-    color: white;
-    placeholder-color: white;
-  }
-
-  .icon-margin {
-    margin-right: 10;
-  }
-
-  .my-button {
-    background-color: #4BD5DC;
-    color: white;
-    font-weight: bold;
-    border-radius: 25;
-    padding-top: 14;
-    padding-bottom: 14;
-    text-transform: uppercase;
-    letter-spacing: 0.1;
-    margin-bottom: 20;
-    margin-top: 10;
-  }
-
-  .auth-buttons {
-    color: white;
-    font-size: 14;
-  }
-
-  .full-height {
-    height: 100%;
-  }
-
-  .logo-container {
-    width: 150;
-    margin-bottom: 60;
-  }
-
-  .container {
-    margin-left: 34;
-    margin-right: 34;
-  }
-  .container1 {
-   background-color: aquamarine;
-  }
+  
 </style>
 
